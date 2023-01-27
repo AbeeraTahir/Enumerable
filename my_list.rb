cb7c66a994185f9fb31353d5b0c5503b6ca07738
Abeera Tahir <abeeratahir2018@gmail.com>
2023-01-28 01:26:08 +0500

    Fix rubocop errors

--- my_list.rb
+++ my_list.rb
@@ -1,18 +1,19 @@
-require_relative './my_enumerable.rb'
+require_relative './my_enumerable'
 
 class MyList
   include MyEnumerable
   def initialize(*args)
     @list = args
   end
-  def each 
-    @list.each { |n| yield n }
+
+  def each(&block)
+    @list.each(&block)
   end
 end
 
-list = MyList.new(1,2,3,4)
+list = MyList.new(1, 2, 3, 4)
 puts(list.all? { |e| e < 5 })
 puts(list.all? { |e| e > 5 })
 puts(list.any? { |e| e == 2 })
 puts(list.any? { |e| e == 5 })
-puts(list.filter { |e| e.even? })
+puts(list.filter(&:even?))
