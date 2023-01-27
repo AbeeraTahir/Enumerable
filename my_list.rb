fbbfbfdb7b36a92e9480a2d000475388d9f36588
Abeera Tahir <abeeratahir2018@gmail.com>
2023-01-28 00:51:00 +0500

    Implement all method

---
+++ my_list.rb
@@ -0,0 +1,15 @@
+require_relative './my_enumerable.rb'
+
+class MyList
+  include MyEnumerable
+  def initialize(*args)
+    @list = args
+  end
+  def each 
+    @list.each { |n| yield n }
+  end
+end
+
+list = MyList.new(1,2,3,4)
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
